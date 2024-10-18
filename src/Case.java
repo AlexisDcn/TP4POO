02436abfa55330452a1e0fb88b2ec2cc46509ba0
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-18 11:14:36 +0200

    commit classe Drone

---
+++ src/Case.java
@@ -0,0 +1,18 @@
+class Case {
+    private int ligne;
+    private int colonne;
+
+    public Case(int ligne, int colonne) {
+        this.ligne = ligne;
+        this.colonne = colonne;
+    }
+
+    // Getters
+    public int getLigne() {
+        return ligne;
+    }
+
+    public int getColonne() {
+        return colonne;
+    }
+}
--- src/Drone.java
+++ src/Drone.java
@@ -1,5 +1,27 @@
-public class Drone {
+import java.util.ArrayList;
+import java.util.List;
 
-    private int numLignes;
-    private int numColonne;
+public class Drone {
+    protected String nom;
+    protected int adresseMAC;
+    protected Case position;
+    protected List<Case> deplacements;
+
+    public Drone(String nom, int adresseMAC) {
+        this.nom = nom;
+        this.adresseMAC = adresseMAC;
+        this.position = new Case((int)(Math.random() * 10), (int)(Math.random() * 10)); 
+        this.deplacements = new ArrayList<>();
+    }
+
+    public void bouger(int numLigne, int numColonne) {
+        if (numLigne >= 0 && numLigne < 10 && numColonne >= 0 && numColonne < 10) {
+            deplacements.add(this.position);
+            this.position = new Case(numLigne, numColonne);
+        } else {
+            System.out.println("Déplacement impossible.");
+        }
+    }
+
+    // Getters et autres méthodes...
 }
